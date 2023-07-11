3f2346b5ea167ababc6209be7035710f77e20831
Decker Ayers <ayersdecker@gmail.com>
2023-07-11 15:43:26 -0400

    Finished CollectionView for now

--- SMARTSign/SMARTSign/MainPage.xaml.cs
+++ SMARTSign/SMARTSign/MainPage.xaml.cs
@@ -1,10 +1,32 @@
-﻿namespace SMARTSign;
+﻿using SMARTSign.Models;
+using System.Collections.ObjectModel;
+
+namespace SMARTSign;
 
 public partial class MainPage : ContentPage
 {
+	public ObservableCollection<ImageCardModel> Cards = new(); 
 	public MainPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
+		LoadCardTesting();
 	}
+
+	public void LoadCardTesting()
+	{
+		ImageCardModel card = new ImageCardModel("Running", "ytidhere", "running.png");
+		for(int i = 0; i < 10; i++) { Cards.Add(card); }
+		ImageCards.ItemsSource = Cards;
+	}
+
+    private void ImageCards_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void Image_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
 
---
+++ SMARTSign/SMARTSign/Models/ImageCardModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMARTSign.Models
+{
+    public class ImageCardModel
+    {
+        public string Name { get; set; }
+        public string YTID { get; set; }
+        public string ImageSource { get; set; }
+        public ImageCardModel(string name, string yTID, string imageSource)
+        {
+            Name = name;
+            YTID = yTID;
+            ImageSource = imageSource;
+        }
+    }
+}
